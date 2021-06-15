da0f304d629ebefbee808333b7c9bcceee388b89
Harsh Patel <harsh.coderc@gmail.com>
2021-06-15 11:47:42 +0530

    Added password encryption in User Model

--- models/user.js
+++ models/user.js
@@ -4,6 +4,10 @@
  */
 
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v4: uuid } = require("uuid");
+const logger = require("../config/logger");
+
 const { ObjectId } = mongoose.Schema;
 
 const UserSchema = mongoose.Schema(
@@ -28,6 +32,14 @@ const UserSchema = mongoose.Schema(
             unique: true,
         },
 
+        encryptedPassword: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+
+        salt: String,
+
         todoItems: [
             {
                 type: ObjectId,
@@ -39,5 +51,38 @@ const UserSchema = mongoose.Schema(
         timestamps: true,
     }
 );
+
+UserSchema.virtual("password")
+    .set(function (password) {
+        this._password = password;
+        this.salt = uuid();
+        this.encryptedPassword = this.securePassword(password);
+    })
+    .get(function () {
+        return this._password;
+    });
+
+UserSchema.methods = {
+    authenticate: function (plainPassword) {
+        return this.securePassword(plainPassword) === this.encryptedPassword;
+    },
+
+    securePassword: function (plainPassword) {
+        if (plainPassword) {
+            try {
+                return crypto
+                    .createHmac("sha256", this.salt)
+                    .update(plainPassword)
+                    .digest("hex");
+            } catch (error) {
+                logger.error("Error while hashing password!!");
+                return "";
+            }
+        } else {
+            logger.error("No password found!!");
+            return "";
+        }
+    },
+};
 
 module.exports = User = mongoose.model("User", UserSchema);
